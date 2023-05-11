729369d819c9db7d4be6df788df62efe7b8de9f9
YiPort <yejinbin1024@163.com>
2023-05-11 23:30:18 +0800

    feat: 博客前端项目，已完成博客的基本功能

---
+++ src/pages/Reward.vue
@@ -0,0 +1,45 @@
+<!-- 赞赏页面 -->
+<template>
+<div>
+	<yp-nav></yp-nav>
+	<div class="container">
+		<el-row :gutter="30">
+			<el-col :sm="24" :md="16" style="transition:all .5s ease-out;margin-bottom:30px;">
+				<yp-reward></yp-reward>
+			</el-col>
+			<el-col :sm="24" :md="8">
+				<yp-rightlist></yp-rightlist>
+			</el-col>
+		</el-row>
+	</div>
+</div>
+</template>
+
+<script>
+import header from '../components/header.vue'
+import rightlist from '../components/rightlist.vue'
+import reward from '../components/reward.vue'
+export default {
+	name: 'Reward',
+	data() { //选项 / 数据
+		return {
+
+		}
+	},
+	methods: { //事件处理器
+
+	},
+	components: { //定义组件
+		'yp-nav': header,
+		'yp-reward': reward,
+		'yp-rightlist': rightlist,
+	},
+	created() { //生命周期函数
+
+	}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/Share.vue
@@ -0,0 +1,46 @@
+<!-- 文章列表 -->
+<template>
+<div>
+	<yp-nav></yp-nav>
+	<div class="container">
+		<el-row :gutter="30">
+			<el-col :sm="24" :md="16" style="transition:all .5s ease-out;margin-bottom:30px;">
+				<yp-articlelist></yp-articlelist>
+			</el-col>
+			<el-col :sm="24" :md="8">
+				<yp-rightlist></yp-rightlist>
+			</el-col>
+		</el-row>
+	</div>
+</div>
+</template>
+
+<script>
+import header from '../components/header.vue'
+import articlelist from '../components/articlelist.vue'
+import rightlist from '../components/rightlist.vue'
+export default {
+	name: "Share",
+	data() { //选项 / 数据
+		return {
+
+		}
+	},
+	methods: { //事件处理器
+
+	},
+	components: { //定义组件
+		'yp-nav': header,
+		'yp-articlelist': articlelist,
+		'yp-rightlist': rightlist,
+	},
+
+	created() { //生命周期函数
+
+	}
+}
+</script>
+
+<style>
+
+</style>
