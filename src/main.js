20324726ea46c223247ddfd37c030200ae2dcd03
YiPort <yejinbin1024@163.com>
2024-03-13 22:09:42 +0800

    style: 调整引入顺序，覆盖element-ui菜单原有样式

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
+import App from './App'
+import router from './router'
 import './assets/css/style.less'
 import store from './store'
 import MavonEditor from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
 // highlight.js代码高亮插件
-import Highlight from './utils/highlight'; // from 路径是highlight.js的路径，纯属自定义
+import Highlight from './utils/highlight';
 import VueViewer, { directive as viewerDirective } from 'v-viewer';
 import 'viewerjs/dist/viewer.css'; // 图片预览器的样式
 // 分页组件
